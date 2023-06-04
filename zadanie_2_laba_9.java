92742c43f683f26e2a7461375d077e98bace35c4
sankkokk <118100738+sankkokk@users.noreply.github.com>
2023-06-04 06:31:02 +0500

    Update zadanie_2_laba_9.java

--- zadanie_2_laba_9.java
+++ zadanie_2_laba_9.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+
+public class zadanie_2_laba_9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input;
+        boolean isValid;
+        do {
+            System.out.print("Введите число в 13-ой системе счисления: ");
+            input = in.nextLine().toUpperCase();
+            isValid = check(input);
+            if (!isValid) {
+                System.out.println("Неверный ввод. Пожалуйста, введите число в 13-ой системе счисления.");
+            }
+        } while (!isValid);
+        String binaryNumber = toBinary(input);
+        System.out.println("Число в 2-ой системе счисления: " + binaryNumber);
+    }
+    private static boolean check(String input) {
+        return input.matches("^[0-9ABC]*$");
+    }
+
+    private static String toBinary(String base13Number) {
+        int decimalNumber = Integer.parseInt(base13Number, 13);
+        return Integer.toBinaryString(decimalNumber);
+    }
+}
 
