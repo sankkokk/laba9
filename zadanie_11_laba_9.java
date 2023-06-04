0a48ea9d566073e5fc051d7d9f611e0472646cea
sankkokk <118100738+sankkokk@users.noreply.github.com>
2023-06-04 06:33:14 +0500

    Update zadanie_11_laba_9.java

--- zadanie_11_laba_9.java
+++ zadanie_11_laba_9.java
@@ -1,1 +1,17 @@
+import java.util.Arrays;
 
+public class zadanie_11_laba_9 {
+    public static void main(String[] args) {
+        String[] strings = {"   Everybody  ", " everybody    ", "   go   to hell   "};
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = strings[i].trim();
+        }
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = strings[i].replaceAll("\s+", " ");
+        }
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = strings[i].substring(0, 1).toUpperCase() + strings[i].substring(1);
+        }
+        System.out.println(Arrays.toString(strings));
+    }
+}
